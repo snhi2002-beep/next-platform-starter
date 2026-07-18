b6f74e126687a01f9cd271a4eea617ee7d6e93f9
snhi2002-beep <snhi2002@gmail.com>
2026-07-18 22:56:48 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,16 +13,9 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <main className="grow">{children}</main>
-                        <Footer />
-                    </div>
-                </div>
+            <body>
+                <p>#page under the development</p>
             </body>
         </html>
     );
